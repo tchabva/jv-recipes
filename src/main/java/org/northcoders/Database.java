d1506a45ae13c9872b9aa1f25f45096ef7af94b1
Tawanda Chabva <tchabva@gmail.com>
2024-11-22 11:10:15 +0000

    feat: created Database Class

---
+++ src/main/java/org/northcoders/Database.java
@@ -0,0 +1,10 @@
+package org.northcoders;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class Database {
+    public static SessionFactory getSessionFactory(){
+        // TODO
+    }
+}
