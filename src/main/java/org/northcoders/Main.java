de2b92b8964f240146366bbd3da393c7efd0c3a7
Tawanda Chabva <tchabva@gmail.com>
2024-11-22 15:57:58 +0000

    feat: main method

--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -1,10 +1,13 @@
 package org.northcoders;
 
 import org.hibernate.SessionFactory;
+import org.northcoders.Entities.*;
 
 public class Main {
     public static void main(String[] args) {
         SessionFactory sessionFactory = Database.getSessionFactory();
         sessionFactory.getSchemaManager().exportMappedObjects(true);
+
+
     }
 }
