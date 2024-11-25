f65a61ed1c38da123ba1569096b208ae41c57938
Tawanda Chabva <tchabva@gmail.com>
2024-11-25 15:24:50 +0000

    feat: entity seminar update

--- src/main/java/org/northcoders/Entities/User.java
+++ src/main/java/org/northcoders/Entities/User.java
@@ -28,5 +28,55 @@ public class User {
     public User() {
     }
 
+    public User(String username, String email, String password, Set<Role> roles, String dateRegistered) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+        this.dateRegistered = dateRegistered;
+    }
 
+    public String getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void setDateRegistered(String dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
 }
