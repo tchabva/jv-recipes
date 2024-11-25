e10950bfc678190e01ed212b16e1666dfad0295a
Tawanda Chabva <tchabva@gmail.com>
2024-11-25 14:33:36 +0000

    feat: comment update

--- src/main/java/org/northcoders/Entities/Comment.java
+++ src/main/java/org/northcoders/Entities/Comment.java
@@ -5,19 +5,72 @@ import jakarta.persistence.*;
 @Entity
 public class Comment {
     @Id
-    @GeneratedValue
-    Long id;
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
 
-    String text;
+    @Lob // Lob = Large Object Binary
+    private String text;
 
-    String datePosted;
+    @Column(name = "date_posted")
+    private String datePosted;
+
+    //You need to specify a join on the "owning side"
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
 
     @ManyToOne
-    Person author;
-
-    @OneToOne
-    Recipe associatedRecipe;
+    @JoinColumn(name = "recipe_id")
+    private Recipe associatedRecipe;
 
     public Comment() {
+    }
+
+    public Comment(Recipe associatedRecipe, User author, String datePosted, String text, Long id) {
+        this.associatedRecipe = associatedRecipe;
+        this.author = author;
+        this.datePosted = datePosted;
+        this.text = text;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(String datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Recipe getAssociatedRecipe() {
+        return associatedRecipe;
+    }
+
+    public void setAssociatedRecipe(Recipe associatedRecipe) {
+        this.associatedRecipe = associatedRecipe;
     }
 }
